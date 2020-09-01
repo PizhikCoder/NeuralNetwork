dcdc81943a4ee63b8f71943fe1691ffa38ee3d22
Павел Мальков <Pashamalkovich@mail.ru>
2020-09-01 22:41:49 +0400

    NeurflNetwork learning

--- NeuralNetwork/NeuralNetwork/Neuron.cs
+++ NeuralNetwork/NeuralNetwork/Neuron.cs
@@ -10,20 +10,35 @@ namespace NeuralNetwork
     public class Neuron
     {
         public List<double> Weights { get; }
+        public List<double> Inputs { get; }
         public NeuronType NeuronType { get; }
         public double Output { get; private set; }
+        public double Delta { get; private set; }
 
         public Neuron(int InputCount, NeuronType type = NeuronType.Normal)
         {
             NeuronType = type;
             Weights = new List<double>();
-            for (int i = 0; i < InputCount ; i++)
+            Inputs = new List<double>();
+            InitWeightsRandomValue(InputCount);
+        }
+
+        private void InitWeightsRandomValue(int InputCount)
+        {
+            var rnd = new Random();
+            for (int i = 0; i < InputCount; i++)
             {
-                Weights.Add(1);
+                Weights.Add(rnd.NextDouble());
+                Inputs.Add(0);
             }
         }
+
         public double FeedForward(List<double> inputs)
         {
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                Inputs[i] = inputs[i];
+            }
             var sum = 0.0;
             for (int i = 0; i < inputs.Count; i++)
             {
@@ -31,7 +46,7 @@ namespace NeuralNetwork
             }
             if (NeuronType != NeuronType.Input)
             {
-            Output = Sigmoid(sum);
+                Output = Sigmoid(sum);
             }
             else
             {
@@ -44,12 +59,33 @@ namespace NeuralNetwork
             var result = 1.0 / (1.0 + Math.Pow(Math.E, -x));
             return result;
         }
+        private double SigmoidDx(double x)
+        {
+            var sigmoid = Sigmoid(x);
+            var result = sigmoid / (1 - sigmoid);
+            return result;
+        }
         public void SetWeights(params double[] weights)
         {
             //TODO: удалить добавления взиожности обучения сети.
             for (int i = 0; i < weights.Length; i++)
             {
                 Weights[i] = weights[i];
+            }
+        }
+        public void Learn(double error, double learningRate)
+        {
+            if (NeuronType == NeuronType.Input)
+            {
+                return;
+            }
+            Delta = error * SigmoidDx(Output);
+            for (int i = 0; i < Weights.Count; i++)
+            {
+                var weight = Weights[i];
+                var input = Inputs[i];
+                var newWeight = weight - input * Delta * learningRate;
+                Weights[i] = newWeight;
             }
         }
         public override string ToString()
